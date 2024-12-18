1ed0067675d7852e50bb32d55c2aea0ca5ccacfa
anathanP <abbasmasoudardakani@gmail.com>
2024-12-18 14:30:06 +0330

    Creating ExpenseFrom Component

---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,85 @@
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  description: z
+    .string()
+    .nonempty("Description is required!")
+    .min(3, "Description needs to be atleast 3 characters!"),
+  amount: z.number({ invalid_type_error: "Amount is required!" }),
+  category: z.number({ invalid_type_error: "You should choose a category" }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+interface Props {
+  categories: string[];
+}
+
+const ExpenseForm = ({ categories }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+  const onSubmit = (data: FieldValues) => console.log(data);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
+        <input
+          {...register("description")}
+          id="description"
+          type="text"
+          className="form-control"
+        />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
+          type="number"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="category">Category</label>
+        <select
+          {...register("category", { valueAsNumber: true })}
+          name="category"
+          id="category"
+          className="form-select"
+        >
+          <option value="">Select a category</option>
+          {categories.map((category, index) => (
+            <option key={index} value={index}>
+              {category}
+            </option>
+          ))}
+        </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default ExpenseForm;
